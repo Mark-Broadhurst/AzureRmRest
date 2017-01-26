53ce4c624b87335364055bcee7ca010bb135fe1e
Gavin van der Merwe <gavin.vandermerwe@chambersandpartners.co.uk>
2017-01-26 20:52:18 +0000

    Added more stub projejcts for examples

---
+++ .examples/2.Create.ResourceGroup.With.AppService/Program.fs
@@ -0,0 +1,17 @@
+﻿
+open System
+open Fake.AzureRm.Env
+open Fake.AzureRm.Auth
+open Fake.AzureRm.Rest
+open Newtonsoft.Json
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    let env = GetEnvironment()
+    let bearerToken = GetToken (env) |> Async.RunSynchronously
+    match bearerToken with
+    | Choice1Of2 x -> printf "%s" (x.ToString()) 
+    | Choice2Of2 (Error(x,y)) -> printf "Fail %s" (x.ToString()) 
+    Console.ReadLine () |> ignore
+    0
